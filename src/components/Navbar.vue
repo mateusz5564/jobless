ce2e1b8cd5931a5d92da62e46a0c642a79e00420
mateusz5564 <mateusz5564@gmail.com>
2019-11-13 21:31:19 +0100

    dodanie firebase, dodanie toolbara z logiem

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="navbar">
+    <v-toolbar color="#004D44" dark height="80px">
+      <v-toolbar-title>
+        <v-img src="@/assets/JoblessLogo.png" height="75px" width="312px"></v-img>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text>Rejestracja</v-btn>
+        <v-btn text>Zaloguj się</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
---
+++ src/firebase/init.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBVNBqTWCAsiKyNu3JX0Z9ZY8yHCK9uDOI",
+  authDomain: "jobless-ef831.firebaseapp.com",
+  databaseURL: "https://jobless-ef831.firebaseio.com",
+  projectId: "jobless-ef831",
+  storageBucket: "jobless-ef831.appspot.com",
+  messagingSenderId: "838595899844",
+  appId: "1:838595899844:web:2e1da08865b8d1137ab491"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+export default firebaseApp.firestore()
