7c83b138fa659c358137b8461fdba618114b2c5d
mateusz5564 <mateusz5564@gmail.com>
2019-11-18 18:20:09 +0100

    poprawka layoutu rejestracji

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -1,94 +1,54 @@
 <template>
-  <div class="register">
-    <v-row justify="center">
-      <v-dialog v-model="dialog" persistent max-width="460px">
-        <v-card>
+  <div class="register d-flex align-center justify-center flex-column">
+    <v-card class="mx-auto" min-width="460px" min-height="470px">
+      <v-tabs v-model="tab" background-color="transparent" grow color="teal">
+        <v-tab>zarejestruj sie</v-tab>
+        <v-tab>dla pracodawcy</v-tab>
+      </v-tabs>
 
-<v-tabs
-      v-model="tab"
-      background-color="transparent"
-      grow
-      color="teal"
-    >
-      <v-tab>
-        zarejestruj sie
-      </v-tab>
-      <v-tab>
-        dla pracodawcy
-      </v-tab>
-    </v-tabs>
+      <v-tabs-items v-model="tab">
+        <!-- PRACOWNIK -->
+        <v-tab-item>
+          <v-card class="ma-4 mt-12" flat>
+            <v-text-field label="Email" outlined color="teal"></v-text-field>
+            <v-text-field label="Hasło" outlined color="teal"></v-text-field>
+            <v-card-actions class="d-flex align-center justify-center mb-12">
+              <v-btn dark color="teal" @click="register">Zarejestruj się</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-tab-item>
 
-    <v-tabs-items v-model="tab">
-      <v-tab-item
-      >
-        <v-card class="ma-4" flat>
-          <v-card-text>jako pracownik</v-card-text>
-          <v-text-field
-            label="Email"
-            outlined
-            color="teal"
-          ></v-text-field>
-          <v-text-field
-            label="Hasło"
-            outlined
-            color="teal"
-          ></v-text-field>
-        </v-card>
-      </v-tab-item>
-
-      <v-tab-item
-      >
-        <v-card class="ma-4" flat>
-          <v-card-text>jako pracodawca</v-card-text>
-          <v-text-field
-            label="Nazwa firmy"
-            outlined
-            color="teal"
-          ></v-text-field>
-          <v-text-field
-            label="Email"
-            outlined
-            color="teal"
-          ></v-text-field>
-          <v-text-field
-            label="Hasło"
-            outlined
-            color="teal"
-          ></v-text-field>
-        </v-card>
-      </v-tab-item>
-    </v-tabs-items>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn text @click="onClose">Zamknij</v-btn>
-            <v-btn text @click="register">Zarejestruj</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-row>
+        <!-- PRACODAWCA -->
+        <v-tab-item>
+          <v-card class="ma-4 mt-12" flat>
+            <v-text-field label="Nazwa firmy" outlined color="teal"></v-text-field>
+            <v-text-field label="Email" outlined color="teal"></v-text-field>
+            <v-text-field label="Hasło" outlined color="teal"></v-text-field>
+            <v-card-actions class="d-flex align-center justify-center mb-12">
+              <v-btn dark color="teal" @click="register">Zarejestruj się</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      dialog: true,
       tab: null
-    }
+    };
   },
   methods: {
-    onClose(){
-      this.dialog = false
-      this.$router.go(-1)
-
+    onClose() {
+      this.dialog = false;
+      this.$router.go(-1);
     },
-    register(){
-
-    }
+    register() {}
   }
-}
+};
 </script>
 
 <style>
