2c842a519269ff2a18456bd831c80428eeb16779
mateusz5564 <mateusz5564@gmail.com>
2019-11-29 21:21:28 +0100

    stworzenie layoutu dla wyszukiwarki

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="home">
     <div class="search_wrapper d-flex justify-center align-center">
-      <v-card class="search_box" height="100px" width="1100px"></v-card>
+      <v-card class="search_box d-flex justify-center align-center" height="100px" width="1100px">
+        <div class="search_content search_box d-flex justify-center align-center">
+          <v-text-field class="search_job_field mr-8" color="teal" label="jakiej pracy szukasz?"></v-text-field>
+          <v-text-field class="search_location_field mr-8" color="teal" label="miejscowość"></v-text-field>
+          <v-btn color="teal" tile dark width="136px" height="40px">
+            <v-icon left>mdi-briefcase-search-outline</v-icon>Szukaj
+          </v-btn>
+        </div>
+      </v-card>
     </div>
     <div class="job_offers mx-auto mt-12">
       <h2 class="mb-3 title">Najnowsze oferty pracy</h2>
@@ -47,6 +55,9 @@ export default {
 .search_box {
   border-radius: 50px;
   opacity: 0.95;
+}
+.search_content{
+  width: 900px;
 }
 .job_offers {
   width: 1100px;
