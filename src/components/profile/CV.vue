8949bc859893f6072dccab9e9917e5f27ba6f1f9
Kamil Bania <kamilbania2@gmail.com>
2019-12-01 00:30:40 +0100

    Wyswietlanie przycisku do tworzenia CV gdy uzytkownik takowego nie posiada

--- src/components/profile/CV.vue
+++ src/components/profile/CV.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-card v-if="this.cv">
+  <v-card v-if="isCVProper()">
     <v-card-title>
       Twoje CV
       <v-spacer></v-spacer>
-      <v-btn text color="green accent-5">Edytuj</v-btn>
+      <v-btn text color="green accent-5" :to="{name: 'cv-kreator', params: {state: 'editing', cv: this.cv}}">Edytuj</v-btn>
     </v-card-title>
 
     <v-list two-line subheader>
@@ -13,9 +13,14 @@
           <v-list-item-content>
             <v-list-item-title>{{ experience.company }}</v-list-item-title>
             <v-list-item-subtitle>
+              <!-- W przypadku timestampa z Firebase 
               {{ new Date(experience.start_date.seconds * 1000).toLocaleString().substring(0, 9) }}
               -
               {{ new Date(experience.end_date.seconds * 1000).toLocaleString().substring(0, 10) }}
+              -->
+              {{ experience.start_date }}
+              -
+              {{ experience.end_date }}
             </v-list-item-subtitle>
             {{ experience.description }}
           </v-list-item-content>
@@ -34,7 +39,13 @@
       </span>
     </v-list>
   </v-card>
-  <v-card v-else>Kreator CV</v-card>
+  <v-card v-else class="text-center">
+    <v-card-title class="justify-center">
+      Nie posiadasz jeszcze CV! <br>
+      Aby je utworzyć kliknij przycisk poniżej
+    </v-card-title>
+    <v-btn large color="success" class="mb-6" :to="{name: 'cv-kreator', params: {state: 'creating'}}">Utwórz CV</v-btn>
+  </v-card>
 </template>
 
 <script>
@@ -42,8 +53,24 @@ import firebase from 'firebase'
 
 export default {
   props: ["cv"],
+  
+  methods: {
+    isCVProper () {      
+      if (!this.cv) {
+        return false
+      }
+
+      if (this.cv.experiences.length === 0 &&
+               this.cv.skills.length === 0) {
+                return false
+      }
+
+      return true
+    }
+  },
 
   created() {
+    /*
     if (!this.cv) {
       let currentUser = firebase.auth().currentUser;
       let database = firebase.firestore();
@@ -62,10 +89,8 @@ export default {
       } else {
         this.user = null;
       }
-    } else if (this.cv.experiences.length === 0 &&
-               this.cv.skills.length === 0) {
-                 this.cv = null
     }
+    */
   }
 };
 </script>
